fc710056b479405b475408cf44b2e6a15d2b1ba8
Drew <epochsolana@gmail.com>
2024-08-21 15:33:39 -0400

    always bust cache

--- packages/app/src/AppRoot.tsx
+++ packages/app/src/AppRoot.tsx
@@ -2,15 +2,10 @@ import React from "react";
 import { App } from "./App";
 import { observer } from "mobx-react";
 import CacheBuster from "react-cache-buster";
-import { version } from "../package.json";
 
 export const AppRoot = observer((): JSX.Element => {
-  let _version = version;
-  if (process.env.ENV === "dev" || process.env.BUST_CACHE === "true") {
-    // random patch version between 0 and 100 to bust cache on every update
-    _version = `0.0.${Math.floor(Math.random() * 100)}`;
-    console.debug(`bust cache with version: ${_version}`);
-  }
+  // random patch version between 0 and 100 to bust cache on every update
+  const _version = `0.0.${Math.floor(Math.random() * 100)}`;
   return (
     <CacheBuster
       currentVersion={_version}
