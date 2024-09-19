583e9c3cf685cc6c04ba1ebefdc3987adf730ab4
Drew <epochsolana@gmail.com>
2024-09-19 07:46:33 -0400

    clean up

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,8 +1,8 @@
 import * as driftVaults from '@drift-labs/vaults-sdk';
 import * as phoenixVaults from '@cosmic-lab/phoenix-vaults-sdk';
 
-export { driftVaults };
-export { phoenixVaults };
+export {driftVaults};
+export {phoenixVaults};
 
 export * from './types';
 export * from './utils';
@@ -14,8 +14,5 @@ export * from './driftWebsocketSubscriber';
 export * from './driftPollingSubscriber';
 export * from './phoenixWebsocketSubscriber';
 export * from './accountLoader';
-export * from './redisClient';
-export * from './proxyClient';
-export * from './txClient';
 export * from './drift';
 export * from './phoenix';
