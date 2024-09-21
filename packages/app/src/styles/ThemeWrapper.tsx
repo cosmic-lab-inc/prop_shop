6d757e7a18448cb0d649dcdc74d6c6bf346bd650
Drew <epochsolana@gmail.com>
2024-09-21 14:23:42 -0400

    snackbar font

--- packages/app/src/styles/ThemeWrapper.tsx
+++ packages/app/src/styles/ThemeWrapper.tsx
@@ -1,43 +1,38 @@
-import React, { FC, ReactNode } from 'react';
-import { customTheme, theme } from './theme';
-import {
-	CssBaseline,
-	styled,
-	StyledEngineProvider,
-	ThemeProvider,
-} from '@mui/material';
-import { MaterialDesignContent, SnackbarProvider } from 'notistack';
+import React, {FC, ReactNode} from 'react';
+import {customTheme, theme} from './theme';
+import {CssBaseline, styled, StyledEngineProvider, ThemeProvider,} from '@mui/material';
+import {MaterialDesignContent, SnackbarProvider} from 'notistack';
 
 const CustomSnack = styled(MaterialDesignContent)(() => ({
-	'&.notistack-MuiContent-success': {
-		fontSize: 20,
-		fontFamily: customTheme.font.heavy,
-		borderRadius: '10px',
-		backgroundColor: customTheme.success,
-	},
-	'&.notistack-MuiContent-error': {
-		fontSize: 20,
-		fontFamily: customTheme.font.heavy,
-		borderRadius: '10px',
-		backgroundColor: customTheme.error,
-	},
+  '&.notistack-MuiContent-success': {
+    fontSize: 20,
+    fontFamily: customTheme.font.light,
+    borderRadius: '10px',
+    backgroundColor: customTheme.success,
+  },
+  '&.notistack-MuiContent-error': {
+    fontSize: 20,
+    fontFamily: customTheme.font.light,
+    borderRadius: '10px',
+    backgroundColor: customTheme.error,
+  },
 }));
 
-export const ThemeWrapper: FC<{ children: ReactNode }> = ({ children }) => {
-	return (
-		<StyledEngineProvider injectFirst>
-			<ThemeProvider theme={theme}>
-				<CssBaseline />
-				<SnackbarProvider
-					Components={{
-						error: CustomSnack,
-						success: CustomSnack,
-					}}
-					autoHideDuration={5000}
-				>
-					{children}
-				</SnackbarProvider>
-			</ThemeProvider>
-		</StyledEngineProvider>
-	);
+export const ThemeWrapper: FC<{ children: ReactNode }> = ({children}) => {
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <SnackbarProvider
+          Components={{
+            error: CustomSnack,
+            success: CustomSnack,
+          }}
+          autoHideDuration={5000}
+        >
+          {children}
+        </SnackbarProvider>
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
 };
