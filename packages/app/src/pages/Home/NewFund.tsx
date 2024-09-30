9a0a6c171f3371c6c117d17598f2e8404d377274
Drew <epochsolana@gmail.com>
2024-09-30 13:03:17 -0400

    format Home

--- packages/app/src/pages/Home/NewFund.tsx
+++ packages/app/src/pages/Home/NewFund.tsx
@@ -1,42 +1,60 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
-import { PropShopClient } from '@cosmic-lab/prop-shop-sdk';
-import { ActionButton } from '../../components';
-import { NewFundDialog } from './NewFundDialog';
+import {Box, Typography} from '@mui/material';
+import {PropShopClient} from '@cosmic-lab/prop-shop-sdk';
+import {ActionButton} from '../../components';
+import {NewFundDialog} from './NewFundDialog';
 
 // todo: fetch vaults and sort by criteria using PropShopClient
-export function NewFund({ client }: { client: PropShopClient }) {
-	const [open, setOpen] = React.useState(false);
+export function NewFund({client}: { client: PropShopClient }) {
+  const [open, setOpen] = React.useState(false);
 
-	return (
-		<>
-			<NewFundDialog
-				client={client}
-				open={open}
-				onClose={() => setOpen(false)}
-			/>
-			<Box
-				sx={{
-					width: '70%',
-					display: 'flex',
-					alignItems: 'center',
-					justifyContent: 'center',
-					flexDirection: 'row',
-					gap: 5,
-				}}
-			>
-				<Typography variant="h2">Think you can beat the market?</Typography>
-				<Box
-					sx={{
-						width: '15%',
-						height: '80px',
-					}}
-				>
-					<ActionButton onClick={() => setOpen(true)}>
-						Create a Fund
-					</ActionButton>
-				</Box>
-			</Box>
-		</>
-	);
+  return (
+    <>
+      <NewFundDialog
+        client={client}
+        open={open}
+        onClose={() => setOpen(false)}
+      />
+      <Box
+        sx={{
+          width: '50%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'row',
+          gap: 5
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'left',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            gap: 1
+          }}
+        >
+          <Typography variant="h2">
+            Beat the market.
+          </Typography>
+          <Typography variant="h2">
+            Rapidly scale your capital.
+          </Typography>
+          <Typography variant="h2">
+            Profit share with investors.
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            width: '30%',
+            height: '90px',
+          }}
+        >
+          <ActionButton onClick={() => setOpen(true)}>
+            Create a Fund
+          </ActionButton>
+        </Box>
+      </Box>
+    </>
+  );
 }
