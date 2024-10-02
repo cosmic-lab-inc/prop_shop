c4d7f26882d5bd907920829f87e1207315f0a136
Drew <epochsolana@gmail.com>
2024-10-02 13:38:24 -0400

    lerna -> pnpm

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -38,6 +38,7 @@ export default defineConfig(({mode}) => {
   return {
     resolve: {
       alias,
+      preserveSymlinks: true
     },
     define: {
       global: "globalThis",
--- packages/sdk/src/types/snack.ts
+++ packages/sdk/src/types/snack.ts
@@ -1,11 +1,4 @@
-import { ReactNode } from 'react';
-
 export type SnackInfo = {
-	variant: 'success' | 'error';
-	message: string;
-};
-
-export type SnackElement = {
-	element: ReactNode;
-	variant: 'success' | 'error';
+  variant: 'success' | 'error';
+  message: string;
 };
