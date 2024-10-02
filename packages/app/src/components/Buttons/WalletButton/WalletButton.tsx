eb6d729cb20d1f6a817029a24e2b9dd3b7ad42c1
Drew <epochsolana@gmail.com>
2024-10-02 17:07:58 -0400

    do not render airdrop if not localnet

--- packages/app/src/components/Buttons/WalletButton/WalletButton.tsx
+++ packages/app/src/components/Buttons/WalletButton/WalletButton.tsx
@@ -1,56 +1,48 @@
 import React from 'react';
-import { BaseWalletMultiButton } from './BaseWalletMultiButton';
-import { PropShopClient } from '@cosmic-lab/prop-shop-sdk';
-import { useSnackbar } from 'notistack';
+import {BaseWalletMultiButton} from './BaseWalletMultiButton';
+import {PropShopClient} from '@cosmic-lab/prop-shop-sdk';
+import {useSnackbar} from 'notistack';
 
 const LABELS = {
-	'change-wallet': 'Switch',
-	connecting: 'Connecting',
-	'copy-address': 'Copy Address',
-	disconnect: 'Disconnect',
-	'has-wallet': 'Connect',
-	'no-wallet': 'Connect',
+  'change-wallet': 'Switch',
+  connecting: 'Connecting',
+  'copy-address': 'Copy Address',
+  disconnect: 'Disconnect',
+  'has-wallet': 'Connect',
+  'no-wallet': 'Connect',
 } as const;
 
 export function WalletButton({
-	client,
-}: {
-	client: PropShopClient | undefined;
+                               client,
+                             }: {
+  client: PropShopClient | undefined;
 }) {
-	const { enqueueSnackbar } = useSnackbar();
+  const {enqueueSnackbar} = useSnackbar();
 
-	async function airdropSol() {
-		if (
-			client &&
-			process.env.ENV === 'dev' &&
-			process.env.RPC_URL === 'http://localhost:8899'
-		) {
-			const snack = await client.airdropSol();
-			enqueueSnackbar(snack.message, {
-				variant: snack.variant,
-			});
-		}
-	}
+  let airdropSol: (() => Promise<void>) | undefined;
+  let airdropUsdc: (() => Promise<void>) | undefined;
+  if (process.env.RPC_URL === 'http://localhost:8899' && client !== undefined) {
+    airdropSol = async () => {
+      const snack = await client.airdropSol();
+      enqueueSnackbar(snack.message, {
+        variant: snack.variant,
+      });
+    };
 
-	async function airdropUsdc() {
-		if (
-			client &&
-			process.env.ENV === 'dev' &&
-			process.env.RPC_URL === 'http://localhost:8899'
-		) {
-			const snack = await client.airdropUsdc();
-			enqueueSnackbar(snack.message, {
-				variant: snack.variant,
-			});
-		}
-	}
+    airdropUsdc = async () => {
+      const snack = await client.airdropUsdc();
+      enqueueSnackbar(snack.message, {
+        variant: snack.variant,
+      });
+    };
+  }
 
-	return (
-		<BaseWalletMultiButton
-			airdropSol={airdropSol}
-			airdropUsdc={airdropUsdc}
-			labels={LABELS}
-			client={client}
-		/>
-	);
+  return (
+    <BaseWalletMultiButton
+      airdropSol={airdropSol}
+      airdropUsdc={airdropUsdc}
+      labels={LABELS}
+      client={client}
+    />
+  );
 }
