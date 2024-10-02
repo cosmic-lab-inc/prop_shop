a56f00277dfddedbd389389f825fb512cb83243b
Drew <epochsolana@gmail.com>
2024-10-02 17:36:42 -0400

    zoom out

--- packages/app/src/pages/Home/Home.tsx
+++ packages/app/src/pages/Home/Home.tsx
@@ -1,23 +1,23 @@
 import * as React from 'react';
-import { Box } from '@mui/material';
-import { Funds } from './Funds';
-import { observer } from 'mobx-react';
-import { NewFund } from './NewFund';
-import { PropShopClient } from '@cosmic-lab/prop-shop-sdk';
+import {Box} from '@mui/material';
+import {Funds} from './Funds';
+import {observer} from 'mobx-react';
+import {NewFund} from './NewFund';
+import {PropShopClient} from '@cosmic-lab/prop-shop-sdk';
 
-export const Home = observer(({ client }: { client: PropShopClient }) => {
-	return (
-		<Box
-			sx={{
-				width: '100%',
-				display: 'flex',
-				alignItems: 'center',
-				flexDirection: 'column',
-				gap: 10,
-			}}
-		>
-			<Funds client={client} />
-			<NewFund client={client} />
-		</Box>
-	);
+export const Home = observer(({client}: { client: PropShopClient }) => {
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        gap: 5,
+      }}
+    >
+      <Funds client={client}/>
+      <NewFund client={client}/>
+    </Box>
+  );
 });
